14f0a0b9756a360992c9807a66ebc6d984e8f4bf
Joshua M. Clulow <jmc@oxide.computer>
2021-10-06 19:34:32 -0700

    buildomat: add basic build/test job mimicking the Actions workflow (#25)

---
+++ .github/buildomat/jobs/build-and-test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#:
+#: name = "illumos / build-and-test"
+#: variety = "basic"
+#: target = "helios"
+#: rust_toolchain = "nightly"
+#: output_rules = []
+#:
+
+set -o errexit
+set -o pipefail
+set -o xtrace
+
+cargo --version
+rustc --version
+
+export RUST_BACKTRACE=1
+
+banner test
+ptime -m cargo test --release --no-fail-fast --verbose --workspace \
+    --features no-linker
