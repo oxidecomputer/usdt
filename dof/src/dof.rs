6282c685cc7a2a3299d4a0b0fb7d5aeacd36d590
Benjamin Naecker <ben@oxide.computer>
2021-11-19 20:26:50 +0000

    Adding Apache-2.0 license file

--- dof/src/dof.rs
+++ dof/src/dof.rs
@@ -4,7 +4,20 @@
 //! contained in an object file. It contains one or more [`Provider`]s, each with one or more
 //! [`Probe`]s. The `Probe` type contains all the information required to locate a probe callsite
 //! within an object file.
+
 // Copyright 2021 Oxide Computer Company
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 use std::mem::size_of;
 use std::{
--- dof/src/lib.rs
+++ dof/src/lib.rs
@@ -26,7 +26,20 @@
 //! Most useful methods and types are exported in the crate root. However, the lower-level Rust
 //! bindings to the raw C-structs are also exposed in the [`dof_bindings`] module, and may be
 //! extracted from a DOF byte slice with the [`des::deserialize_raw_sections`] function.
+
 // Copyright 2021 Oxide Computer Company
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #[cfg(feature = "des")]
 pub mod des;
