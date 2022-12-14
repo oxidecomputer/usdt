bf40d198fd557bd6423f1e4a93e5caabb081a5fc
Adam H. Leventhal <ahl@oxide.computer>
2022-12-14 12:55:08 -0800

    more

--- common-build.rs
+++ common-build.rs
@@ -19,7 +19,9 @@ fn main() {
         println!("cargo:rustc-cfg=usdt_stable_asm");
     }
     #[cfg(target_os = "macos")]
-    if version_check::supports_feature("asm_sym").unwrap_or(false) {
+    if version_check::supports_feature("asm_sym").unwrap_or(false)
+        && !version_check::is_min_version("1.67").unwrap_or(false)
+    {
         println!("cargo:rustc-cfg=need_asm_sym");
     }
 }
